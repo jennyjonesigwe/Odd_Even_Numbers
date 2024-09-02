79db0c736aa047969174437d6b640b15983feb65
jennyjonesigwe <jennyjonesigwe@gmail.com>
2024-09-02 14:39:38 +0100

    Odd and Even Numbers

---
+++ Odd_Even_Numbers.js
@@ -0,0 +1,14 @@
+
+const number = 2;
+
+if(number % 2 == 0) {
+  console.log(number + " is an Even number" + ".");
+  // console.log("This is an Even number" + ".");
+}
+
+else {
+  console.log(number + " is an Odd number" + ".");
+  // console.log("This is an Odd number" + ".");
+}
+
+
