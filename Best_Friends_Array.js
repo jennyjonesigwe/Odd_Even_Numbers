e3352530cffa14144d909008e0beec15017f5802
jennyjonesigwe <125607473+jennyjonesigwe@users.noreply.github.com>
2024-09-11 15:54:57 +0100

    Delete Best_Friends_Array.js

--- Best_Friends_Array.js
+++
@@ -1,3 +0,0 @@
-const friends = ["Cynthia Oluchi", "Prof Oluwasegun Ayanloye",  "Oluwatobi Awoniyi",  "Kenneth Nkwocha", "Babatunde AF" +"."]
-
-console.log(friends + " These are the names of my best friends.")
