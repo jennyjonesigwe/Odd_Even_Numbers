86429bdf211ae2785f2b00846fa9eae5d30447b5
jennyjonesigwe <125607473+jennyjonesigwe@users.noreply.github.com>
2024-09-11 15:57:29 +0100

    Create Best_Friends_Array.js

---
+++ Best_Friends_Array.js
@@ -0,0 +1,3 @@
+const friends = ["Cynthia Oluchi", "Prof Oluwasegun Ayanloye",  "Oluwatobi Awoniyi",  "Kenneth Nkwocha", "Babatunde AF" +"."]
+
+console.log(friends + " These are the names of my best friends.")
